95a42a252cbf149ab79c05c179bd0e0f8bf29ba3
Dananjaya Ramanayake <dananjaya@cynetis-embedded.com>
2021-04-13 08:38:38 +0200

    Initial commit

---
+++ src/BowlingGame/Game.java
@@ -0,0 +1,49 @@
+package BowlingGame;
+
+public class Game {
+    private final int[] rolls = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pins) {
+        rolls[currentRoll++] = pins;
+    }
+
+    public int score() {
+        int score = 0;
+        int firstInFrame = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(rolls[firstInFrame])) {
+                score += 10 + nextTwoBallsForStrike(firstInFrame);
+                firstInFrame++;
+            } else if (isSpare(firstInFrame)) {
+                score += 10 + nextBallForSpare(firstInFrame);
+                firstInFrame += 2;
+            } else {
+                score += twoBallsInFrame(firstInFrame);
+                firstInFrame += 2;
+            }
+
+        }
+        return score;
+    }
+
+    private boolean isStrike(int roll) {
+        return roll == 10;
+    }
+
+    private int twoBallsInFrame(int firstInFrame) {
+        return rolls[firstInFrame] + rolls[firstInFrame + 1];
+    }
+
+    private int nextBallForSpare(int firstInFrame) {
+        return rolls[firstInFrame + 2];
+    }
+
+    private int nextTwoBallsForStrike(int firstInFrame) {
+        return rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
+    }
+
+    private boolean isSpare(int firstInFrame) {
+        return rolls[firstInFrame] + rolls[firstInFrame + 1] == 10;
+    }
+}
